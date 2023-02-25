e25a36adf80d28708853d1de2992a76bab6a5e43
johan53 <96956391+johan53@users.noreply.github.com>
2023-02-25 15:17:07 -0600

    Implement Auto 2 Cone Cycle and Parking
    Co-Authored-By: GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HighRightAutoSleeveCycle.java
@@ -0,0 +1,262 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(name = "High Right Auto Cycle w/ Park")
+public class HighRightAutoSleeveCycle extends LinearOpMode {
+    public static double speed = 1200; // Arbitrary number; static to allow for analyzing how PID performs through multiple speeds in dashboard
+    public static PIDCoefficients pidCoeffs = new PIDCoefficients(0, 0, 0); // PID coefficients that need to be tuned probably through FTC dashboard
+    final double CLAW_OPEN = 0;
+    final double CLAW_CLOSE = 0.4;
+    final double CLAW_ROTATE_UP = 0.73;
+    final double CLAW_ROTATE_DOWN = 0.1;
+    // Name of the Webcam to be set in the config
+    private final String webcamName = "Webcam 1";
+    public PIDCoefficients pidGains = new PIDCoefficients(0, 0, 0); // PID gains which we will define later in the process
+    ElapsedTime PIDTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS); // Timer
+    // Hardware
+    private Hardware hardware;
+    private SleeveDetection sleeveDetection;
+    private OpenCvCamera camera;
+    private Servo clawServo;
+    private Servo twistServo;
+    private DcMotorEx elevatorMotor;
+    private DcMotorEx armMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Init hardware
+        hardware = new Hardware(hardwareMap);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+        sleeveDetection = new SleeveDetection();
+        camera.setPipeline(sleeveDetection);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+
+        clawServo = hardwareMap.get(Servo.class, "Servo");
+        twistServo = hardwareMap.get(Servo.class, "twist");
+        elevatorMotor = hardwareMap.get(DcMotorEx.class, "elevatorMotor");
+        armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
+
+        while (!isStarted()) {
+            telemetry.addData("ROTATION: ", sleeveDetection.getPosition());
+            telemetry.addData("Path", "Moving");
+            telemetry.addData("fr ticks", hardware.frontRightMotor.getCurrentPosition());
+            telemetry.addData("fl ticks", hardware.frontLeftMotor.getCurrentPosition());
+            telemetry.addData("br ticks", hardware.backRightMotor.getCurrentPosition());
+            telemetry.addData("bl ticks", hardware.backLeftMotor.getCurrentPosition());
+            telemetry.addData("arm ticks", hardware.armMotor.getCurrentPosition());
+            telemetry.addData("elevator ticks", elevatorMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        waitForStart();
+        // hardware.armMotor.setPositionPIDFCoefficients(20);
+        // hardware.armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        SleeveDetection.ParkingPosition position = sleeveDetection.getPosition();
+
+        // The cycle
+        hardware.clawServo.setPosition(CLAW_CLOSE);
+        sleep(500);
+        hardware.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.armMotor.setTargetPosition(675);
+        hardware.armMotor.setPower(0.5);
+        hardware.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (hardware.armMotor.isBusy()) {
+            twistServo.setPosition(CLAW_ROTATE_UP);
+            telemetry.addData("Arm", "Moving");
+            telemetry.addData("arm ticks", hardware.armMotor.getCurrentPosition());
+            telemetry.update();
+        }
+        hardware.armMotor.setPower(0);
+
+        // Go forward
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setAllWheelMotorTargetPosition(2300);
+        setAllWheelMotorPower(0.6);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (hardware.frontRightMotor.isBusy() || hardware.frontLeftMotor.isBusy() || hardware.backRightMotor.isBusy() || hardware.backLeftMotor.isBusy()) {
+            elevatorMotor.setPower(0.7);
+            sleep(1500);
+            elevatorMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            hardware.armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        elevatorMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hardware.armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Rotate right
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        hardware.frontRightMotor.setTargetPosition(900);
+        hardware.backRightMotor.setTargetPosition(900);
+        hardware.frontLeftMotor.setTargetPosition(-900);
+        hardware.backLeftMotor.setTargetPosition(-900);
+
+        setAllWheelMotorPower(0.25);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        trackAllWheelCurrentPositionTelemetry();
+        setAllWheelMotorPower(0);
+
+        // Strafe left
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        hardware.frontRightMotor.setTargetPosition(-525);
+        hardware.backRightMotor.setTargetPosition(525);
+        hardware.frontLeftMotor.setTargetPosition(525);
+        hardware.backLeftMotor.setTargetPosition(-525);
+        setAllWheelMotorPower(0.25);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        trackAllWheelCurrentPositionTelemetry();
+        setAllWheelMotorPower(0);
+
+        // Go forward/line up with junction
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setAllWheelMotorTargetPosition(160);
+        setAllWheelMotorPower(0.5);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        trackAllWheelCurrentPositionTelemetry();
+        setAllWheelMotorPower(0);
+
+        // Arm cycle starts
+        hardware.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.armMotor.setTargetPosition(-300);
+        hardware.armMotor.setPower(0.75);
+        hardware.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (armMotor.isBusy()) {
+            telemetry.addData("moving", "moving");
+            telemetry.update();
+        }
+        hardware.armMotor.setPower(0);
+
+        hardware.clawServo.setPosition(CLAW_OPEN);
+        sleep(1000);
+
+        hardware.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.armMotor.setTargetPosition(500);
+        hardware.armMotor.setPower(0.75);
+        hardware.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (armMotor.isBusy()) {
+            telemetry.addData("moving", "moving");
+            telemetry.update();
+        }
+        hardware.armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Rotate left to park
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.frontRightMotor.setTargetPosition(-1200);
+        hardware.backRightMotor.setTargetPosition(-1200);
+        hardware.frontLeftMotor.setTargetPosition(1200);
+        hardware.backLeftMotor.setTargetPosition(1200);
+        setAllWheelMotorPower(0.5);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        trackAllWheelCurrentPositionTelemetry();
+        setAllWheelMotorPower(0);
+
+        // move backwards in parking
+        setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setAllWheelMotorTargetPosition(-225);
+        setAllWheelMotorPower(0.5);
+        setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+        trackAllWheelCurrentPositionTelemetry();
+        setAllWheelMotorPower(0);
+
+        // The parking after the cycle
+        if (position == SleeveDetection.ParkingPosition.LEFT) {
+            telemetry.addData("left", "4324");
+            // Strafe left
+            setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            hardware.frontRightMotor.setTargetPosition(750);
+            hardware.backRightMotor.setTargetPosition(-750);
+            hardware.frontLeftMotor.setTargetPosition(-750);
+            hardware.backLeftMotor.setTargetPosition(750);
+
+            setAllWheelMotorPower(1);
+            setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+            trackAllWheelCurrentPositionTelemetry();
+            setAllWheelMotorPower(0);
+        } else if (position == SleeveDetection.ParkingPosition.CENTER) {
+            telemetry.addData("center", "4324");
+            // Strafe left
+            setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            hardware.frontRightMotor.setTargetPosition(450);
+            hardware.backRightMotor.setTargetPosition(-450);
+            hardware.frontLeftMotor.setTargetPosition(-450);
+            hardware.backLeftMotor.setTargetPosition(450);
+
+            setAllWheelMotorPower(1);
+            setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+            trackAllWheelCurrentPositionTelemetry();
+            setAllWheelMotorPower(0);
+        } else if (position == SleeveDetection.ParkingPosition.RIGHT) {
+            telemetry.addData("right", "4324");
+            // Strafe right
+            setAllWheelMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            hardware.frontRightMotor.setTargetPosition(-1700);
+            hardware.backRightMotor.setTargetPosition(1700);
+            hardware.frontLeftMotor.setTargetPosition(1700);
+            hardware.backLeftMotor.setTargetPosition(-1700);
+
+            setAllWheelMotorPower(1);
+            setAllWheelMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
+            trackAllWheelCurrentPositionTelemetry();
+            setAllWheelMotorPower(0);
+        }
+    }
+
+    public void setAllWheelMotorPower(double power) {
+        hardware.frontRightMotor.setPower(power);
+        hardware.frontLeftMotor.setPower(power);
+        hardware.backRightMotor.setPower(power);
+        hardware.backLeftMotor.setPower(power);
+    }
+
+    public void setAllWheelMotorMode(DcMotor.RunMode mode) {
+        hardware.frontRightMotor.setMode(mode);
+        hardware.frontLeftMotor.setMode(mode);
+        hardware.backRightMotor.setMode(mode);
+        hardware.backLeftMotor.setMode(mode);
+    }
+
+    public void setAllWheelMotorTargetPosition(int position) {
+        hardware.frontRightMotor.setTargetPosition(position);
+        hardware.backRightMotor.setTargetPosition(position);
+        hardware.frontLeftMotor.setTargetPosition(position);
+        hardware.backLeftMotor.setTargetPosition(position);
+    }
+
+    public void trackAllWheelCurrentPositionTelemetry() {
+        while (hardware.frontRightMotor.isBusy() || hardware.frontLeftMotor.isBusy() || hardware.backRightMotor.isBusy() || hardware.backLeftMotor.isBusy()) {
+            telemetry.addData("Path", "Moving");
+            telemetry.addData("fr ticks", hardware.frontRightMotor.getCurrentPosition());
+            telemetry.addData("fl ticks", hardware.frontLeftMotor.getCurrentPosition());
+            telemetry.addData("br ticks", hardware.backRightMotor.getCurrentPosition());
+            telemetry.addData("bl ticks", hardware.backLeftMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
